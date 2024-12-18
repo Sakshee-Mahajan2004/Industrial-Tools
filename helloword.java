a31273dd1e92a0482e85d6afb9096ed98c4f51d5
Sakshee-mahajan2004 <mahajansakshee21178@gmail.com>
2024-12-18 17:11:27 +0530

    add new text

--- helloword.java
+++ helloword.java
@@ -1,6 +1,8 @@
 public class helloword {
     public static void main(String args[]) {
         System.out.println("hello pavan");
+        System.out.println("my name is Pavan");
+        System.out.println("pratoksha mam");
     }
 
 }
