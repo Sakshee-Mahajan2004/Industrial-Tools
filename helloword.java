5a048ae8be67d907b372823c222daf2dcb4e8371
Sakshee-mahajan2004 <mahajansakshee21178@gmail.com>
2024-12-18 17:18:43 +0530

    change name pratiksha

--- helloword.java
+++ helloword.java
@@ -2,7 +2,7 @@ public class helloword {
     public static void main(String args[]) {
         System.out.println("hello pavan");
         System.out.println("my name is Pavan");
-        System.out.println("pratoksha mam");
+        System.out.println("pratiksha mam");
     }
 
 }
